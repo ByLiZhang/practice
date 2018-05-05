cfc942a261ced2b127b869c1bc0dcc1dec845ffb
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-05-05 12:27:11 -0700

    Cleaned up vowelCount

--- vowelCount.js
+++ vowelCount.js
@@ -1,19 +1,3 @@
-var newString = '';
-var index = 0;
-while ( index < test_array.length ) {
-  newString += test_array[index][0] + test_array[index][test_array[index].length-1];     //test_array.slice(-1);
-  index ++;
-}
-
-
-var newArray = [];
-for (var i= 0; i < test_array.length; i++) {
-  if (vowelCount(test_array[i]) > 1) {
-    newArray.push(test_array[i]);
-  }
-}
-
-
 function vowelCount(string){
   var counter = 0;
   var vowel = 'aeiou';
@@ -27,10 +11,11 @@ function vowelCount(string){
 
 function otherVowelCount(string){
   var vowels = 'aeiouAEIOU'
-  var count = 0;
+  var counter = 0;
   for( var letterIndex =0; letterIndex < string.length; letterIndex++){
     if( vowels.indexOf(string[letterIndex])!==-1){
-      count++;
+      counter++;
     }
   }
+  return counter;
 }
