834469a6e605e93ed29afc45eb29d0bd43e35a5c
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-05-05 12:18:43 -0700

    Finished pancakeSort

--- pancakeSort.js
+++ pancakeSort.js
@@ -1,55 +1,32 @@
-var testArr = [3,5,2,1,7,6,4];
-
 function pancakeSort(arr) {	
 	for (var i = arr.length-1; i >= 1; i--) {
 		var max = arr[0];
 		var max_index = 0;
-		for (var j = 1; j < i; j++) {
+		for (var j = 1; j <= i; j++) {  //find out the current max and its index
 			if (arr[j] > max){
 				max = arr[j];
 				max_index = j;
 			}
 		}
-        if (max_index === 0) {
+
+        if (max_index === i) {  //check if the current max is already at the correct position 
             continue;
         }
 
-		if (max_index > 0 && max_index !== i) {
-			var flipper = arr.splice(0, max_index+1);
-			arr = flipper.reverse().concat(arr);  //max is at the top now
-			flipper = arr.splice(0, i+1);
-			arr = flipper.reverse().concat(arr);  
+        var flipper;
+		if (max_index > 0) {  //when the current max is not at the first position, flip it to the top
+			flipper = arr.splice(0, max_index+1);
+			arr = flipper.reverse().concat(arr);  //the current max is at the top now
 		}
+
+		flipper = arr.splice(0, i+1);
+		arr = flipper.reverse().concat(arr);  //flip the current stack so that the max goes to the right position
 	}
 	return arr;
 }
 
+var testArr = [3,5,2,9,1,7,6,4,8];  //sample array 
 var result = pancakeSort(testArr);
 result;
 
-
-// var testArr = [3,5,2,1,7,6,4];
-
-// function pancakeSort(arr) {	
-// 	for (var i = arr.length-1; i >= 1; i--) {
-// 		var max = arr[0];
-// 		var max_index = 0;
-// 		for (var j = 1; j < i; j++) {
-// 			if (arr[j] > max){
-// 				max = arr[j];
-// 				max_index = j;
-// 			}
-// 		}
-// 		if (max_index > 0 && max_index !== i) {
-// 			var flipper = arr.splice(0, max_index+1);
-// 			arr = flipper.reverse().concat(arr);  //max is at the top now
-// 			flipper = arr.splice(0, i+1);
-// 			arr = flipper.reverse().concat(arr);  
-// 		}
-// 	}
-// 	return arr;
-// }
-
-// var result = pancakeSort(testArr);
-// result;
 
